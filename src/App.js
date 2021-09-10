411ab7fc64f35bc4abf1903cdd8dbdbf9ff6e6c4
Lance Turbes <waywardpooch@users.noreply.github.com>
2021-09-10 07:02:02 -0700

    read instructions; started modifying componenets

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [swData, setSwData] = useState([]);
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(axios.get().then().catch().finally(), []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
